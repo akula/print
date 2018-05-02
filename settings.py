ecd13418cfc798188b1562eefecf3f482848194f
akula <jijinger@gmail.com>
2018-05-03 06:30:55 +0800

    add settings file for option

---
+++ settings.py
@@ -0,0 +1,6 @@
+# 配置文件 参数 printerX_com 为连接该打印机的对应串口号, controller_com 为连接数据发送控制器的串口号,可在控制面板中查看.修改保存后就重启程序生效.
+ 
+printer1_com = 'COM1'
+printer2_com = 'COM2'
+printer3_com = 'COM3'
+controller_com = 'COM6'
